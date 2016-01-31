22744db69e94bee2ade6015d26ab908733be603d
Alejandro Ramírez Muñoz <alejandrorm45@yahoo.com>
2016-01-30 22:55:13 -0600

    isspace(), isdigit(), isalpha() removidos e implementados manualmente, correjida matriz de salidas para no usar fill_n

--- Lexico.h
+++ Lexico.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 
 using namespace std;
 
@@ -15,18 +14,16 @@ enum Estado {
 enum Entrada {
     E0, E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15,
     NUMERO_ENTRADAS
-    /*
-        E0 Espacio      E4 "        E8 ;        E12 !
-        E1 Letra        E5 + -      E9 $        E13 =
-        E2 Digito       E6 * /      E10 |       E14 < >
-        E3 .            E7 ( )      E11 &       E15 ASCII (restante)
-    */
 } ;
 
 enum Token {
     IDENTIFICADOR, ENTERO, REAL, CADENA, OP_ADICION, OP_MULTIPLICACION,
     PARENTESIS, DELIMITADOR, FIN_ENTRADA, OP_LOGICO, OP_ASIGNACION,
     OP_RELACIONAL, ERROR
+};
+
+enum Salida {
+    NO, SI
 };
 
 class Lexico {
@@ -52,7 +49,29 @@ private:
         {K, K, K, K, K, K, K, K, K, K, K, K, K, Q18, K, K},
         {K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K}
     };
-    int salidas[NUMERO_ESTADOS][NUMERO_ENTRADAS];
+     /** Estando es un estado terminal, la entrada determina si el token a finalizado */
+    int salidas[NUMERO_ESTADOS][NUMERO_ENTRADAS] = {
+        {NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO},
+        {SI, NO, NO, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {SI, SI, NO, NO, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO},
+        {SI, SI, NO, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, NO, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, NO, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, NO, SI, SI},
+        {SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI, SI},
+        {NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO}
+    };
 
     char caracter;
     int estado;
@@ -108,11 +127,12 @@ private:
     }
 
     int transicion( char c ) {
-        if( isspace( c ) ) {
+        if(  c == ' ' || c == '\t' || c == '\v' || c == '\f' ||
+                c == '\r' || c == '\n' ) {
             return E0;
-        } else if( isalpha( c ) ||  c == '_' ) {
+        } else if( ( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' ) ||  c == '_' ) {
             return E1;
-        } else if( isdigit( c ) ) {
+        } else if( c >= '0' && c <= '9' ) {
             return E2;
         } else if( c == '.' ) {
             return E3;
@@ -145,38 +165,12 @@ private:
 
 public:
     Lexico() {
-        fill_n( salidas[Q0],  NUMERO_ENTRADAS, -1 );
-        fill_n( salidas[Q1],  NUMERO_ENTRADAS, IDENTIFICADOR );
-        salidas[Q1][E1] = salidas[Q1][E2] = -1;
-        fill_n( salidas[Q2],  NUMERO_ENTRADAS, ENTERO );
-        salidas[Q2][E2] = salidas[Q2][E3] = -1;
-        fill_n( salidas[Q3],  NUMERO_ENTRADAS, -1 );
-        fill_n( salidas[Q4],  NUMERO_ENTRADAS, REAL );
-        salidas[Q3][E2] = -1;
-        fill_n( salidas[Q5],  NUMERO_ENTRADAS, -1 );
-        fill_n( salidas[Q6],  NUMERO_ENTRADAS, CADENA );
-        fill_n( salidas[Q7],  NUMERO_ENTRADAS, OP_ADICION );
-        fill_n( salidas[Q8],  NUMERO_ENTRADAS, OP_MULTIPLICACION );
-        fill_n( salidas[Q9],  NUMERO_ENTRADAS, PARENTESIS );
-        fill_n( salidas[Q10], NUMERO_ENTRADAS, DELIMITADOR );
-        fill_n( salidas[Q11], NUMERO_ENTRADAS, FIN_ENTRADA );
-        fill_n( salidas[Q12], NUMERO_ENTRADAS, -1 );
-        fill_n( salidas[Q13], NUMERO_ENTRADAS, OP_LOGICO );
-        fill_n( salidas[Q14], NUMERO_ENTRADAS, -1 );
-        fill_n( salidas[Q15], NUMERO_ENTRADAS, OP_LOGICO );
-        salidas[Q15][E13] = -1;
-        fill_n( salidas[Q16], NUMERO_ENTRADAS, OP_ASIGNACION );
-        salidas[Q16][E13] = -1;
-        fill_n( salidas[Q17], NUMERO_ENTRADAS, OP_RELACIONAL );
-        salidas[Q17][E13] = -1;
-        fill_n( salidas[Q18], NUMERO_ENTRADAS, OP_RELACIONAL );
-
-        error = false;
-
         archivo.open( "entrada.txt" );
 
         if ( !archivo.is_open() ) {
             error = true;
+        } else {
+            error = false;
         }
     }
 
@@ -197,7 +191,7 @@ public:
 
         while( true ) {
             columna = transicion( archivo.peek() );
-            if( salidas[estado][columna] != -1 ) {
+            if( salidas[estado][columna] == SI ) {
                 break;
             }
             estado = matriz[estado][columna];
